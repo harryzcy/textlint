1593d6c43cea540faf59c0de2bc3186b9595792d
github-action-benchmark <github@users.noreply.github.com>
2025-06-24 01:57:09 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8288cf3d1f23bbd597b68e44c1c3d937f3860003

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750644310640,
+  "lastUpdate": 1750730229795,
   "repoUrl": "https://github.com/harryzcy/textlint",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 0.5909110699,
             "unit": "seconds",
             "range": "± 0.020588229000000013"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8288cf3d1f23bbd597b68e44c1c3d937f3860003",
+          "message": "chore(deps): update eslint to ^8.34.1 (#1597)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2025-06-24T00:28:07Z",
+          "tree_id": "dcc05a5e0d4100d57628e0e9d8e6b3a1df06e459",
+          "url": "https://github.com/harryzcy/textlint/commit/8288cf3d1f23bbd597b68e44c1c3d937f3860003"
+        },
+        "date": 1750730228833,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "npm run bench:self",
+            "value": 0.21644792956,
+            "unit": "seconds",
+            "range": "± 0.034410461"
+          },
+          {
+            "name": "npm run bench:technical-writing",
+            "value": 1.7577948627600002,
+            "unit": "seconds",
+            "range": "± 0.027832155999999886"
+          },
+          {
+            "name": "npm run bench:jtf-style",
+            "value": 0.5945786771600001,
+            "unit": "seconds",
+            "range": "± 0.01063578799999998"
           }
         ]
       }
