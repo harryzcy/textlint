b5d05b8dd567f30f92b6e51b506296afe0f75140
github-action-benchmark <github@users.noreply.github.com>
2026-02-08 17:06:03 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1d66439991a816b90ec6f45a717221705faf070b

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770558361031,
+  "lastUpdate": 1770570363254,
   "repoUrl": "https://github.com/harryzcy/textlint",
   "entries": {
     "Benchmark": [
@@ -16128,6 +16128,48 @@ window.BENCHMARK_DATA = {
             "value": 0.52182132318,
             "unit": "seconds",
             "range": "± 0.011541940999999944"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d66439991a816b90ec6f45a717221705faf070b",
+          "message": "fix(deps): update patch updates",
+          "timestamp": "2026-02-08T17:04:23Z",
+          "tree_id": "9f81848af303b487f45a0631f8b57a1ab95b9ef0",
+          "url": "https://github.com/harryzcy/textlint/commit/1d66439991a816b90ec6f45a717221705faf070b"
+        },
+        "date": 1770570362405,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "npm run bench:self",
+            "value": 0.20701192092,
+            "unit": "seconds",
+            "range": "± 0.04319999900000002"
+          },
+          {
+            "name": "npm run bench:technical-writing",
+            "value": 1.6861089565200005,
+            "unit": "seconds",
+            "range": "± 0.018576661999999855"
+          },
+          {
+            "name": "npm run bench:jtf-style",
+            "value": 0.5869414693199999,
+            "unit": "seconds",
+            "range": "± 0.009461739999999996"
           }
         ]
       }
