38b4180ae7593c9d64e6fbe18f53b420b6bbbf0c
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 22:01:17 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3748d3d7f4101abbe11a79eca6736a55f19e6394

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774908073412,
+  "lastUpdate": 1774908077675,
   "repoUrl": "https://github.com/harryzcy/textlint",
   "entries": {
     "Benchmark": [
@@ -20622,6 +20622,48 @@ window.BENCHMARK_DATA = {
             "value": 0.5902247308400002,
             "unit": "seconds",
             "range": "± 0.02345799500000001"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3748d3d7f4101abbe11a79eca6736a55f19e6394",
+          "message": "chore(deps): update dependency diff to v5.2.2 [security]",
+          "timestamp": "2026-03-30T21:59:28Z",
+          "tree_id": "2a14ce0ab6abba05cf525eb6194f4776c28944a9",
+          "url": "https://github.com/harryzcy/textlint/commit/3748d3d7f4101abbe11a79eca6736a55f19e6394"
+        },
+        "date": 1774908075467,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "npm run bench:self",
+            "value": 0.21426378844,
+            "unit": "seconds",
+            "range": "± 0.04215337499999999"
+          },
+          {
+            "name": "npm run bench:technical-writing",
+            "value": 1.8241125704399999,
+            "unit": "seconds",
+            "range": "± 0.10519005299999984"
+          },
+          {
+            "name": "npm run bench:jtf-style",
+            "value": 0.6022758742400001,
+            "unit": "seconds",
+            "range": "± 0.019186783999999957"
           }
         ]
       }
